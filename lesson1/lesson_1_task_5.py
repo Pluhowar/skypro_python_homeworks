7692493ade39163ced94e5fb6b36615f2a10ca8b
Pluhowar <Evgenui_the_best@mail.ru>
2024-12-12 16:06:12 +0300

    one_lason

---
+++ lesson1/lesson_1_task_5.py
@@ -0,0 +1,24 @@
+num = input()
+num1 = num
+num = input()
+num2 = num
+num = input()
+num3 = num
+num = input()
+num4 = num
+num = input()
+num5 = num
+num = input()
+num6 = num
+num = input()
+num7 = num
+num = input()
+num8 = num
+num = input()
+num9 = num
+num = input()
+num10 = num
+num = input()
+num11 = num
+num = input()
+print(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11)
